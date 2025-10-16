a9968c6e7f93c22c0f4fe258d7ea8c1ac1d0d2d3
Marcelle <marcellearaujobri@gmail.com>
2025-10-16 18:07:08 +0100

    create project with dependencies

---
+++ src/main/java/com/brisapets/webapp/BrisaPets/BrisaPetsApplication.java
@@ -0,0 +1,13 @@
+package com.brisapets.webapp.BrisaPets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrisaPetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrisaPetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brisapets/webapp/BrisaPets/BrisaPetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brisapets.webapp.BrisaPets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrisaPetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
